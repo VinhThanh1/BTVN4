7fdd9f7c2a9b0fc1322c7530318fe1332a3e6147
Your Name <you@example.com>
2023-07-25 14:34:28 +0700

    Fast commit

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-//Bai1
-// function chialaydu(a, b) {
-//     const divisionResult = Math.floor(a / b);
-//     const remainder = a % b;
-//     return { divisionResult, remainder };
-//   }
-//   const result = chialaydu(17, 4);
-//   console.log(result);
+// Bai1
+function chialaydu(a, b) {
+    const divisionResult = Math.floor(a / b);
+    const remainder = a % b;
+    return { divisionResult, remainder };
+  }
+  const result = chialaydu(17, 4);
+  console.log(result);
 
-//Bai2
+// Bai2
 function fruits() {
     return ["apple", "banana", "orange", "kiwi", "mango"];
   }
@@ -31,3 +31,28 @@ function fruits() {
   console.log("First fruit:", firstFruit); // Output: "First fruit: apple"
   
 //Bai3
+// A
+var sum = [1, 2, 3, 4, 5, 6];
+for (let i = 0; i < sum.length; i++) {
+  console.log(sum[i]);
+}
+//B
+var sum = [1, 2, 3, 4, 5, 6];
+var totalSum = 0;
+var i = 0;
+while (i < sum.length) {
+  totalSum += sum[i];
+  i++;
+}
+console.log("Total sum:", totalSum);
+
+// Bai 4
+var a = 8;
+var b = 6;
+var c = a - b;
+
+if (c >= 2) {
+  console.log("Result of c:", c);
+} else {
+  console.log("Result of c is smaller than 2. Please re-enter numbers a and b.");
+}
